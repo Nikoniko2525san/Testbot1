6e77b53f7ce2a6bc21828332dc9454a20c68948d
Nikoniko2525san <nikoniko.nikosanki@icloud.com>
2025-02-19 20:00:24 +0900

    index.js を更新

--- index.js
+++ index.js
@@ -13,19 +13,33 @@ const client = new Client(config);
 app.use(express.json());
 app.use(middleware(config));
 
+// Webhookリクエストを受け取る
 app.post("/webhook", (req, res) => {
   const events = req.body.events;
-  Promise.all(events.map(handleEvent)).then(() => res.sendStatus(200));
+
+  Promise.all(events.map(handleEvent))
+    .then(() => res.sendStatus(200))
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send("Internal Server Error");
+    });
 });
 
+// メッセージを処理する
 async function handleEvent(event) {
   if (event.type !== "message" || event.message.type !== "text") {
     return Promise.resolve(null);
   }
-  return client.replyMessage(event.replyToken, {
-    type: "text",
-    text: `「${event.message.text}」って言いましたね！`,
-  });
+  
+  try {
+    await client.replyMessage(event.replyToken, {
+      type: "text",
+      text: `「${event.message.text}」って言いましたね！`,
+    });
+  } catch (error) {
+    console.error("Message reply failed", error);
+    throw new Error("Message reply failed");
+  }
 }
 
 app.listen(3000, () => console.log("Server running on port 3000"));
